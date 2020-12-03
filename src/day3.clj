0d115d588f2c6c76d5184ab0865dd897694925e0
lyderichti59 <lyderic.dutillieux@origenial.fr>
2020-12-03 23:46:57 +0100

    Enhanced day3 version

--- src/day3.clj
+++ src/day3.clj
@@ -7,20 +7,17 @@
 ;; COMMON
 (defn tree?
   "Returns true if there is a tree at this line with the given slope"
-  [line index]
+  [[index line]]
   (= \# (nth line (mod index (count line)))))
 
 (defn count-trees [[x y]]
   (with-open [rdr (io/reader "./inputs/day3.txt")]
-    (loop [trees 0,
-           remaining-lines (line-seq rdr),
-           pos  0]
-      (if-let [line (first remaining-lines)]
-        (recur (cond-> trees (tree? line pos) inc)
-               (nthrest remaining-lines y)
-               (+ pos x))
-        trees))))
-
+    (let [x-positions (iterate (partial + x) 0)], ;;Sequence of x positions
+      (->> (line-seq rdr)
+        (take-nth y)              ;;Skip lines y by y
+        (map vector x-positions)  ;;zip x position with each line
+        (filter tree?)            ;;Keep lines with trees
+        count))))                 ;;Count
 
 (defn -main []
   (pprint [(count-trees [3 1])
